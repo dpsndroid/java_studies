89ad6b874fe9555c57f378e260c810757076ebff
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-08-03 14:47:47 -0300

    Update_01

---
+++ PrimeiroProjeto/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+        System.out.println();
+        System.out.println("One more test to make");
+        System.out.println();
+        System.out.println("It's only the beginning");
+        System.out.println();
+    }
+}
