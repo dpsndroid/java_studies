30edcc414b870c4979299e54fe80ab4acb1ea6a5
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-08-04 20:55:48 -0300

    Update_aug04

---
+++ PrimeiroProjeto/src/treino.java
@@ -0,0 +1,6 @@
+public class treino {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+        System.out.println("let me try something diferent");
+    }
+}
