f794ee5815a8d218355d25dbe36c440b1a3e9cc6
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-09-14 15:17:13 -0300

    update_sep14

---
+++ Variables/src/Store7.java
@@ -0,0 +1,25 @@
+public class Store7 {
+    // instance fields
+    String productType;
+    
+    // constructor method
+    public Store7(String product) {
+      productType = product;
+    }
+    
+    // advertise method
+    public void advertise() {
+          System.out.println("Selling " + productType + "!");
+      System.out.println("Come spend some money!");
+    }
+    
+    // main method
+    public static void main(String[] args) {
+      Store7 lemonadeStand = new Store7("Lemonade");
+      lemonadeStand.advertise();
+      lemonadeStand.advertise();
+      lemonadeStand.advertise();
+  
+      
+    }
+  }
