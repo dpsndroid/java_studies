57ae4713fd718524d6ba7004885b4273322d3498
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-09-27 16:33:41 -0300

    update_sep27

---
+++ Variables/src/Playground.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Playground {
+
+	public static void main(String[] args) {
+  
+    for (int i = 0; i < 28; i++) {
+  		System.out.println("Congrats on finishing Java loops!");
+	  }
+    int j = 0;
+    while (j < 50){
+      System.out.println("I've finishe this tiring exercise #" + j);
+      j++;
+    }
+  }
+
+}
