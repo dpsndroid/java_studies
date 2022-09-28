510d584eb56d1c68f4d13b078da8589b1e4972b8
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-09-28 18:50:21 -0300

    update_sep28

---
+++ Review.java
@@ -0,0 +1,18 @@
+public class Review {
+  
+    public static void main(String[] args) {
+      
+      String name = "Daniel Nepomuceno";
+  
+      System.out.println(name.length());
+      System.out.println(name.concat(" Oi!"));
+      System.out.println(name.indexOf("N"));
+      System.out.println(name.charAt(10));
+      System.out.println(name.equals("Eduardo"));
+      System.out.println(name.toUpperCase());
+      System.out.println(name.toLowerCase());
+      
+      
+    }
+    
+  }
